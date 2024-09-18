0c9996d3e5267189b1c37fa511d9b944793a977d
nayakanaufal <nayaka_aziz_33rpl@student.smktelkom-mlg.sch.id>
2024-09-18 11:58:18 +0700

    Add files via upload

---
+++ PerulanganWhile.java
@@ -0,0 +1,11 @@
+package Perulangan;
+
+public class PerulanganWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
